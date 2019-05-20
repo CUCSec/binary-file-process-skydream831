b4528b4fe685203c3e00553ed284e533aa2bdbf0
homework-python <1097799587@qq.com>
2019-05-20 22:54:33 +0800

    第二次作业

--- Conan.py
+++ Conan.py
@@ -2,7 +2,28 @@ import struct
 
 
 def tamper(student_id):
-  pass
+  with open('lenna.bmp','r+b') as f:
+    f.seek(54)
+    i=1
+    for a in student_id:
+      if int(a)==0:
+        if i==1:
+          f.read(10*3) 
+          f.write(b'\x00\x00\x00')
+          i+=1
+        else:
+          f.read((10-1)*3) 
+          f.write(b'\x00\x00\x00')
+          i+=1
+      else:
+        if i==1:
+          f.read(int(a)*3)
+          f.write(b'\x00\x00\x00')
+          i+=1
+        else:
+          f.read((int(a)-1)*3)
+          f.write(b'\x00\x00\x00')
+          i+=1
 
 
 def detect():
